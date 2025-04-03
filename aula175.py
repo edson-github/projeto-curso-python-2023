e3dab0d782dfb8377523a3bec87eddf34727b951
edson-github <edson.maxiad@gmail.com>
2025-04-03 15:34:54 -0300

    incluso arquivos das aulas 176 e 177 em 20250403

--- aula175.py
+++ aula175.py
@@ -23,7 +23,15 @@
 # False         false
 # None          null
 
-
+{
+    "title": "O Senhor dos Anéis: A Sociedade do Anel",
+    "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+    "is_movie": False,
+    "imdb_rating": 8.8,
+    "year": 2001,
+    "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+    "budget": True
+}
 
 
 
