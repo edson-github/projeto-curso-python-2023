4e2c736297d6c469189f8ec7ff93989c8dcadcae
edson-github <edson.maxiad@gmail.com>
2025-04-03 15:12:38 -0300

    insercao de conteudo da aula 151 JSON

---
+++ aula151.py
@@ -0,0 +1,30 @@
+# Arquivos JSON - JavaScript Object Notation
+
+# É um formato de troca de dados entre sistemas e programas muito leve e fácil de ser lido
+# por seres humanos e máquinas.
+
+# O Python possui um módulo integrado chamado json para trabalhar com JSON.
+
+# O módulo json facilita a leitura e a escrita de dados no formato JSON.
+
+from queue import Full
+
+
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": False,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": True
+}
+
+
+
+
+
+
+
+
+
